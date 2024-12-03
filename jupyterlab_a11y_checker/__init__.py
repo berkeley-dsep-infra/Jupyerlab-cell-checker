74b9e5e0bc6b4d01bdcba8750f820a0c1c75afbc
Chanbin Park <chanbin.park@berkeley.edu>
2024-12-03 12:58:52 -0800

    Add alt-text generation feature

--- jupyterlab_a11y_checker/__init__.py
+++ jupyterlab_a11y_checker/__init__.py
@@ -1,8 +1,16 @@
 from ._version import __version__
-
-
+    
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
         "dest": "jupyterlab-a11y-checker"
     }]
+    
+def _jupyter_server_extension_paths():
+    return [{
+        "module": "jupyterlab_a11y_checker"
+    }]
+
+def load_jupyter_server_extension(app):
+    from .handlers import start_ollama
+    start_ollama()
