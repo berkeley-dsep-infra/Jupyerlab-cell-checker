403e5b8fe2bb92c0df9b022bbcfd98ce4c0d69f1
Chanbin Park <chanbin.park@berkeley.edu>
2024-12-18 22:02:01 -0800

    Implement dynamic url for user ollama endpoint

--- jupyterlab_a11y_checker/__init__.py
+++ jupyterlab_a11y_checker/__init__.py
@@ -1,18 +1,5 @@
-from .handlers import setup_handlers
-    
 def _jupyter_labextension_paths():
     return [{
         "src": "labextension",
         "dest": "jupyterlab-a11y-checker"
     }]
-    
-# def _jupyter_server_extension_points():
-#     return [{ "module": "jupyterlab_a11y_checker" }]
-
-# def _load_jupyter_server_extension(server_app):
-#     server_app.log.info("jupyterlab_a11y_checker server extension is being loaded.")
-#     print("jupyterlab_a11y_checker server extension is being loaded.")
-#     setup_handlers(server_app.web_app)
-    
-    
-# load_jupyter_server_extension = _load_jupyter_server_extension
