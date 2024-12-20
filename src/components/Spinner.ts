d18ad5f6239dcc5873cdbdab75718bd25cb853e5
Chanbin Park <chanbin.park@berkeley.edu>
2024-12-19 23:04:45 -0800

    Add comments to the code  and make minor changes

--- src/components/Spinner.ts
+++ src/components/Spinner.ts
@@ -6,10 +6,12 @@ const spinnerOptions = {
   length: 20, // Length of each line
   width: 10, // Thickness of each line
   radius: 25, // Radius of the inner circle
-  scale: 0.2, // Scales the whole spinner
+  scale: 0.1, // Scales the whole spinner
   corners: 1, // Corner roundness (0..1)
   color: '#154F92', // Spinner color
   fadeColor: 'transparent', // Fade color
+  top: '50%',
+  bottom: '50%',
   speed: 1, // Rounds per second
   rotate: 0, // Rotation offset
   animation: 'spinner-line-fade-quick', // CSS animation
